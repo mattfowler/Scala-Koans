cf81dcd3b0f159cf0b8efea14fb4a92da588371d
matthewfowler <matt.fow@gmail.com>
2019-06-19 21:15:17 -0400

    Adding a sequential suite to force ordering of tests.

---
+++ src/test/scala/org/functionalkoans/forscala/Koans.scala
@@ -0,0 +1,41 @@
+package org.functionalkoans.forscala
+
+import org.functionalkoans.forscala.support.KoanSuite
+import org.scalatest.Sequential
+
+import scala.collection.immutable.IndexedSeq
+
+class Koans extends Sequential {
+  override val nestedSuites: IndexedSeq[KoanSuite] =
+    IndexedSeq(
+      new AboutAsserts,
+      new AboutValAndVar,
+      new AboutConstructors,
+      new AboutTuples,
+      new AboutLists,
+      new AboutMaps,
+      new AboutSets,
+      new AboutSequencesAndArrays,
+      new AboutMutableMaps,
+      new AboutMutableSets,
+      new AboutOptions,
+      new AboutPatternMatching,
+      new AboutCaseClasses,
+      new AboutHigherOrderFunctions,
+      new AboutPartiallyAppliedFunctions,
+      new AboutPartialFunctions,
+      new AboutForExpressions,
+      new AboutEnumerations,
+      new AboutEmptyValues,
+      new AboutParentClasses,
+      new AboutNamedAndDefaultArguments,
+      new AboutInfixPrefixAndPostfixOperators,
+      new AboutInfixTypes,
+      new AboutTypeSignatures,
+      new AboutTraits,
+      new AboutPreconditions,
+      new AboutUniformAccessPrinciple,
+      new AboutImplicits,
+      new AboutInteroperability,
+    )
+}
