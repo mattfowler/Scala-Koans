6edd21baf0958536dcfcdf8b2f876bfdc311e372
matthewfowler <matt.fow@gmail.com>
2019-06-22 09:21:44 -0400

    Adding docstrings and removing assert tests as they are not used much in the project.

--- src/test/scala/org/functionalkoans/forscala/AboutAsserts.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutAsserts.scala
@@ -3,28 +3,33 @@ package org.functionalkoans.forscala
 import org.scalatest.Matchers
 import support.KoanSuite
 
-// meditate on AboutAsserts to see how the Scala Koans work
+/**
+  * Lesson 1 - About Asserts
+  *
+  * In this lesson we'll learn about assertions . An assertion will assert if a condition is true,
+  * if it is, the test will pass and we can move on to the next one.  If it does not pass the test
+  * will fail and we need to fix it.
+  *
+  * Most tests will have a double underscore in them.  This is a cue to you to fill the underscore
+  * in with the correct value to make the test pass.  A test will show as 'pending' until you
+  * do so.
+  *
+  * Sometimes we will give you a test that is failing and ask you to make it pass. In this case
+  * the test will fail and say 'TestFailedException' until you make the code pass.
+  *
+  * Usually we'll use Scalatest's should matchers for this. Should matchers take the form of:
+  *
+  * actualValue should be(expectedValue)
+  *
+  */
 class AboutAsserts extends KoanSuite with Matchers {
 
-  koan("asserts can take a boolean argument") {
-    assert(false) // should be true
-  }
-
-  koan("asserts can include a message") {
-    assert(false, "This should be true")
-  }
-
-  koan("true and false values can be compared with should matchers") {
+  koan("We can use should matchers to do assertions") {
     true should be(__) // should be true
   }
 
-  koan("booleans in asserts can test equality") {
-    val v1 = 4
-    val v2 = 4
-    v1 === __ // === is an assert. It is from ScalaTest, not from the Scala language
+  koan("Sometimes we present you with a failing test that you should make pass") {
+    4 should be (3)
   }
 
-  koan("sometimes we expect you to fill in the values") {
-    assert(__ == 1 + 1)
-  }
 }
